7dad91a9b1dd4b853a943e194ba79ecbf81cd46d
Paure <paure@student.42lyon.fr>
2022-02-02 15:14:34 +0100

    day01 - ex02

---
+++ day01/ex02/oddeven.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?php
+
+while(1)
+{
+    print("Entrez un nombre: ");
+    $line = fgets(STDIN);
+    if ($line === FALSE)
+        break;
+    $line = trim($line);
+    if (is_numeric($line) === TRUE)
+    {
+            if ($line % 2 === 0)
+                print("Le chiffre $line est Pair\n");
+            else
+                print("Le chiffre $line est Impair\n");
+    }
+    else
+        print ("'$line' n'est pas un chiffre\n");
+}
+
+?>
